89c812033efab683a41e5c35413ca286a7dea349
Judah Goldstein <39227048+JudahGoldstein@users.noreply.github.com>
2021-05-28 00:29:17 -0400

    ass1 fib done

--- Assignment1/Fibonacci/src/test.java
+++ Assignment1/Fibonacci/src/test.java
@@ -1,8 +1,6 @@
 public class test {
     public static void main(String[] args) {
         Fibonacci f = new Fibonacci();
-        for(int x = 0; x < 20; x++) {
-            System.out.println(f.compute(x));
-        }
+        System.out.println(f.compute(27));
     }
 }
